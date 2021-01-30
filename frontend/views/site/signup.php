e68e02eb8204e8fce2532f623c36a2e65bf38bde
bayugustiparaya <bayu.gustip7820@gmail.com>
2021-01-30 22:42:30 +0700

    add input name

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -19,7 +19,11 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'nama_depan')->textInput(['autofocus' => true]) ?>
+                
+                <?= $form->field($model, 'nama_belakang') ?>
+
+                <?= $form->field($model, 'username') ?>
 
                 <?= $form->field($model, 'email') ?>
 
